c587251f84b9e6e1814394718fa7c10842fd2486
swdevss <swouda@snelstart.nl>
2017-09-20 15:55:39 +0200

    Explicitly handle parent and child resource names in resource uri

--- SnelStart.B2B.Client/Operations/CrudOperationsWithParentBase.cs
+++ SnelStart.B2B.Client/Operations/CrudOperationsWithParentBase.cs
@@ -3,21 +3,28 @@ using System.Threading.Tasks;
 
 namespace SnelStart.B2B.Client.Operations
 {
-    internal class CrudOperationsWithParentBase<T> : ICrudOperationsWithParent<T> where T : IIdentifierModel
+    internal abstract class CrudOperationsWithParentBase<T> : ICrudOperationsWithParent<T> where T : IIdentifierModel
     {
         protected readonly ClientState ClientState;
         protected readonly string ResourceName;
+        protected readonly string ParentResourceName;
 
-        public CrudOperationsWithParentBase(ClientState clientState, string resourceName)
+        protected CrudOperationsWithParentBase(ClientState clientState, string parentResourceName, string resourceName)
         {
             ClientState = clientState;
+            ParentResourceName = parentResourceName;
             ResourceName = resourceName;
         }
         
-        public Task<Response<T>> CreateAsync(Guid parentId, T dto) => ClientState.ExecutePostAsync(string.Format(ResourceName, parentId), dto);
-        public Task<Response<T>> UpdateAsync(Guid parentId, T dto) => ClientState.ExecutePutAsync(string.Format(ResourceName, parentId), dto);
-        public Task<Response<T>> GetByIdAsync(Guid parentId, Guid id) => ClientState.ExecuteGetByIdAsync<T>(string.Format(ResourceName, parentId), id);
-        public Task<Response> DeleteAsync(Guid parentId, Guid id) => ClientState.ExecuteDeleteAsync(string.Format(ResourceName, parentId), id);
-        public Task<Response> DeleteAsync(Guid parentId, T dto) => ClientState.ExecuteDeleteAsync(string.Format(ResourceName, parentId), dto.Id);
+        public Task<Response<T>> CreateAsync(Guid parentId, T dto) => ClientState.ExecutePostAsync(GetBaseUri(parentId), dto);
+        public Task<Response<T>> UpdateAsync(Guid parentId, T dto) => ClientState.ExecutePutAsync(GetBaseUri(parentId), dto);
+        public Task<Response<T>> GetByIdAsync(Guid parentId, Guid id) => ClientState.ExecuteGetByIdAsync<T>(GetBaseUri(parentId), id);
+        public Task<Response> DeleteAsync(Guid parentId, Guid id) => ClientState.ExecuteDeleteAsync(GetBaseUri(parentId), id);
+        public Task<Response> DeleteAsync(Guid parentId, T dto) => ClientState.ExecuteDeleteAsync(GetBaseUri(parentId), dto.Id);
+
+        protected string GetBaseUri(Guid parentId)
+        {
+            return $"{ParentResourceName}/{parentId}/{ResourceName}";
+        }
     }
 }
