49702e0ceb191b686f1177c3b47560d5b816608f
Peter de Bruine <p.debruine@ridder.nl>
2018-03-01 16:48:52 +0100

    Added inkoopboekingen

---
+++ SnelStart.B2B.Client/Operations/Inkoopboekingen/InkoopboekingenOperations.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnelStart.B2B.Client.Operations
+{
+    internal class InkoopboekingenOperations : CrudOperationsBase<InkoopboekingModel>, IInkoopboekingenOperations
+    {
+        public InkoopboekingenOperations(ClientState clientState) : base(clientState, InkoopboekingModel.ResourceName) { }
+    }
+}
