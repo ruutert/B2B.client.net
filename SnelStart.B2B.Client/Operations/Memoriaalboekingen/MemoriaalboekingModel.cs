b8c8c748e85fd8e3295d9a8fbde6e5b0bfa5b668
Tom van de Graaf <tvandegraaf@snelstart.nl>
2018-03-05 16:29:31 +0100

    Made KasboekingModel public and added some documentation.

--- SnelStart.B2B.Client/Operations/Memoriaalboekingen/MemoriaalboekingModel.cs
+++ SnelStart.B2B.Client/Operations/Memoriaalboekingen/MemoriaalboekingModel.cs
@@ -4,13 +4,19 @@ using SnelStart.B2B.Client.Operations;
 
 namespace SnelStart.B2B.Client.Operations
 {
-    
+    /// <summary>
+    /// De gegevenscontainer voor een memoriaalboeking.
+    /// </summary>
     public class MemoriaalboekingModel : IdentifierModel
     {
         /// <summary>
-        /// 
+        /// Geeft de naam van deze gegevenscontainer terug.
         /// </summary>
         public const string ResourceName = "memoriaalboekingen";
+        
+        /// <summary>
+        /// Geeft een instantie van een <see cref="MemoriaalboekingModel"/> terug.
+        /// </summary>
         public MemoriaalboekingModel() : base(ResourceName)
         {
         }
