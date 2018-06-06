b31e23ee1498f97200456b021fdc301cac8fdeda
Len Goenga <lgoenga@snelstart.nl>
2018-06-06 09:20:36 +0200

    Inkoopfactuur UBL uploaden

--- SnelStart.B2B.Client/Operations/Inkoopboekingen/IInkoopboekingenOperations.cs
+++ SnelStart.B2B.Client/Operations/Inkoopboekingen/IInkoopboekingenOperations.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SnelStart.B2B.Client.Operations
 {
     public interface IInkoopboekingenOperations : ICrudOperations<InkoopboekingModel>
     {
+        Task<Response<InkoopboekingModel>> UploadUblAsync(UblContentModel id, CancellationToken cancellationToken);
     }
 }
